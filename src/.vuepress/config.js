6a2b7e63ce0023bee4c1eedf6987d40e660300d9
Taoning Ge <tony.bazhang@gmail.com>
2021-02-04 21:43:31 +0800

    first commit

---
+++ src/.vuepress/config.js
@@ -0,0 +1,94 @@
+const { description } = require('../../package');
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: '高中笔记',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: '分享我的高中笔记',
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: 'https://github.com/gtn1024/Notes',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdated: true,
+    nav: [
+      {
+        text: '首页',
+        link: '/'
+      },
+      {
+        text: '语文',
+        link: '/chi/'
+      },
+      {
+        text: '生物',
+        link: '/bio/'
+      },
+      {
+        text: '地理',
+        link: '/geo/'
+      }
+    ],
+    sidebar: {
+      '/chi/': [
+        {
+          title: '专题',
+          collapsable: false,
+          children: [
+            '论证特点',
+            {
+              title: '小说',
+              collapsable: true,
+              children: ['小说总述', '赏析小说环境描写的文字', '情节表达技巧']
+            }
+          ]
+        }
+      ],
+      '/bio/': [],
+      '/geo/': [
+        {
+          title: '区域地理',
+          collapsable: false,
+          children: ['中亚', '东南亚']
+        }
+      ]
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/nprogress',
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+    [
+      '@vuepress/active-header-links',
+      {
+        sidebarLinkSelector: '.sidebar-link',
+        headerAnchorSelector: '.header-anchor'
+      }
+    ]
+  ]
+};
---
+++ src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: https://img-1251985644.file.myqcloud.com/images/Rcad637bb62ac6bf1ea5863e4bfac7073.png
+tagline: 右上角有学科哦~
+# actionText: Quick Start →
+# actionLink: /guide/
+# features:
+#   - title: Feature 1 Title
+#     details: Feature 1 Description
+#   - title: Feature 2 Title
+#     details: Feature 2 Description
+#   - title: Feature 3 Title
+#     details: Feature 3 Description
+footer: Made by Taoning with ❤️
+---
